2df6f8dfc7c68f53f2455494284fa5c6f3553c4c
Carlos Chau <carlos.chau719@gmail.com>
2025-04-16 00:50:19 +0800

    Add credit, switch px to rem for font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Box, Button, Container, CssBaseline, Snackbar, TextField, Typography } from '@mui/material'
+import { Box, Button, Container, CssBaseline, Link, Snackbar, TextField, Typography } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { generateHkid, generateHkidBatch } from './hkid-utils'
 import SourceSansPro from './assets/font/source-sans-pro-latin-400-normal.woff2';
@@ -55,18 +55,19 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Box sx={{position: 'absolute', top:'1em', right: '1em'}}><Typography color='primary' fontSize={'1.3rem'}>Credit: <Link href='https://pinkylam.me/playground/hkid/'>Ice Lam</Link></Typography></Box>
       <Container sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-        <Typography sx={{ fontSize: '28px', width: '360px', margin: '0 80px 30px 80px ', fontWeight: '300', maxWidth: '320px', lineHeight:'1.3'}} color='primary'>Hong Kong Identity Card Number Generator</Typography>
+        <Typography sx={{ fontSize: '1.75rem', width: '360px', margin: '0 80px 30px 80px ', fontWeight: '300', maxWidth: '320px', lineHeight:'1.3'}} color='primary'>Hong Kong Identity Card Number Generator</Typography>
         <Box sx={{cursor: 'pointer', display:'flex', flexWrap: 'wrap'}} onClick={() =>{
           navigator.clipboard.writeText(hkidList.join("\n"));
           setOpenSnackBar(true);
         }} >
           {/* <Typography sx={{ fontSize: '45px', fontFamily: 'Source Sans Pro, sans-serif', letterSpacing: '6px', fontWeight: '700' }} color='primary' >{hkid}</Typography> */}
-          {hkidList.map((hkid, i) => <Typography key={hkid+i} sx={{ fontSize: '35px', fontFamily: 'Source Sans Pro, sans-serif', letterSpacing: '6px', fontWeight: '700', margin:'0 15px'}} color='primary' >{hkid}</Typography>)}
+          {hkidList.map((hkid, i) => <Typography key={hkid+i} sx={{ fontSize: '2.188rem', fontFamily: 'Source Sans Pro, sans-serif', letterSpacing: '6px', fontWeight: '700', margin:'0 15px'}} color='primary' >{hkid}</Typography>)}
         </Box>
         <Box sx={{ marginTop: '40px'}}>
-          <Box><TextField variant='standard'sx={{ input: {fontSize:'40px', textAlign: 'center', color:'white'}, maxWidth: '200px'}} size='small' type='number' color='primary' onChange={e=> setNum(parseInt(e.target.value))} value={num}></TextField></Box>
-        <Button variant='outlined' size='large' sx={{ textTransform:'none', borderWidth: '3px', borderColor: 'white', marginTop:'10px', fontSize: '20px', padding:'15px 36px', minWidth:'70px', lineHeight: 1 }} onClick={() => {
+          <Box><TextField variant='standard'sx={{ input: {fontSize:'2.5rem', textAlign: 'center', color:'white'}, maxWidth: '200px'}} size='small' type='number' color='primary' onChange={e=> setNum(parseInt(e.target.value))} value={num}></TextField></Box>
+        <Button variant='outlined' size='large' sx={{ textTransform:'none', borderWidth: '3px', borderColor: 'white', marginTop:'10px', fontSize: '1.25rem', padding:'15px 36px', minWidth:'70px', lineHeight: 1 }} onClick={() => {
           setHkidList(generateHkidBatch(num))
         }}>
           Generate
